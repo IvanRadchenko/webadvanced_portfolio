b639938314f470c271d8582c669a8213c9d89c74
Ivan Radchenko <radchenk@gmail.com>
2016-10-03 00:05:53 +0300

    About me part. Several elements: social, quuotes and forms recoded in Flexbox

--- gulp/tasks/copy.image.js
+++ gulp/tasks/copy.image.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
   $.gulp.task('copy:image', function() {
-    return $.gulp.src(['./source/img/{header,login,preloader,slider,sprite,water_map}/*.*','./source/img/work/*.*','./source/img/work/leaves/leaves.png'], { since: $.gulp.lastRun('copy:image') })
+    return $.gulp.src(['./source/img/{header,login,preloader,slider,sprite,water_map}/*.*','./source/img/work/*.*','./source/img/work/forestback/*.*','./source/img/work/imgsprite.png'], { since: $.gulp.lastRun('copy:image') })
       .pipe($.gulp.dest($.config.root + '/assets/img'));
   });
 };
--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -4,12 +4,12 @@ module.exports = function() {
   $.gulp.task('sprite', function () {
     var spriteData = $.gulp.src('./source/img/work/leaves/*.png')
       .pipe($.gp.spritesmith({
-        imgName: 'leaves.png',
-        cssName: '_leaves.scss',
+        imgName: 'imgsprite.png',
+        cssName: '_imgsprite.scss',
         padding: 20,
         algorithm: 'top-down'
       }));
-    spriteData.img.pipe($.gulp.dest('./source/img/work/leaves/'));
+    spriteData.img.pipe($.gulp.dest('./source/img/work/'));
     spriteData.css.pipe($.gulp.dest('./source/style/common/'));
 
     return spriteData;
