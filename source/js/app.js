f582bb720a64ee674156ceafe34fe4c3f03c2105
Ivan Radchenko <radchenk@gmail.com>
2016-10-04 11:28:15 +0300

    Add map section

--- source/js/app.js
+++ source/js/app.js
@@ -1,7 +1,12 @@
-// (function() {
-//   'use strict';
-//
-//   setTimeout(function() {
-//     document.querySelector('.greating_picture').classList.add('m--show');
-//   }, 1000);
-// })();
+//Google Map
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+};
