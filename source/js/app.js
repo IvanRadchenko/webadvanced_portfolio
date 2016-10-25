e8993399cdb1e76e0e21decfd9f2db41f083d2f9
Ivan Radchenko <radchenk@gmail.com>
2016-10-25 10:56:05 +0300

    Styles - Add the overlay final animation styles, menu animation base

--- source/js/app.js
+++ source/js/app.js
@@ -3,15 +3,16 @@
 var menuModule = (function () {
   function mainMenu() {
     var elem = document.querySelector(".overlay"),
-        hamburger = document.querySelector('.hamburger');
+        hamburger = document.querySelector('.hamburger'),
+		menuList = document.querySelector('.menu__list');
 
 
     hamburger.addEventListener('click', function(e) {
-
 			e.preventDefault();
 
       hamburger.classList.toggle('hamburger--cross');
-			elem.classList.toggle('overlay--isactive');
+		  elem.classList.toggle('overlay--isactive');
+		menuList.classList.toggle('menu__list--open');
 	});
 	}
 
