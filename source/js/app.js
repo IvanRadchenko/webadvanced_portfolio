a72c087c1d7d3d45c922203896f1b720a4eec8f5
Ivan Radchenko <radchenk@gmail.com>
2016-10-26 03:59:30 +0300

    Add menu animation, transitions and js

--- source/js/app.js
+++ source/js/app.js
@@ -1,30 +1,77 @@
 "use strict";
 
-var menuModule = (function () {
-  function mainMenu() {
-    var elem = document.querySelector(".overlay"),
-        hamburger = document.querySelector('.hamburger'),
-		menuList = document.querySelector('.menu__list');
+// var menuModule = (function () {
+//   function mainMenu() {
+//     var elem = document.querySelector(".overlay"),
+//         hamburger = document.querySelector('.hamburger'),
+// 		menuList = document.querySelector('.menu__list'),
+// 		menuItems = document.querySelectorAll('.menu__item');
+//
+//
+//     hamburger.addEventListener('click', function(e) {
+// 			e.preventDefault();
+//
+//       hamburger.classList.toggle('hamburger--cross');
+// 		  elem.classList.toggle('overlay--isactive');
+// 		menuList.classList.toggle('menu__list--open');
+//
+// 		menuItems.forEach(function(i) {
+// 			var item = menuItems[i];
+// 			setTimeout(function() {
+// 				console.log(item);
+// 				item.classList.add('menu__item--animate-in');
+// 			}, 500 * i);
+// 		});
+// 	});
+// 	}
+//
+//
+// 	function blogMenu() {
+// 		console.log(overlayRight);
+// 	}
+//
+// 	 return {
+//         mainMenu: mainMenu,
+//         blogMenu: blogMenu
+//     };
+// })();
+//
+// menuModule.mainMenu();
 
+var menuModule = {
+	mainMenu: function() {
+		var elem = document.querySelector(".overlay"),
+			hamburger = document.querySelector('.hamburger'),
+			menuList = document.querySelector('.menu__list'),
+			menuItems = document.querySelectorAll('.menu__item');
 
-    hamburger.addEventListener('click', function(e) {
+		hamburger.addEventListener('click', function(e) {
 			e.preventDefault();
 
-      hamburger.classList.toggle('hamburger--cross');
-		  elem.classList.toggle('overlay--isactive');
-		menuList.classList.toggle('menu__list--open');
-	});
+			hamburger.classList.toggle('hamburger--cross');
+			elem.classList.toggle('overlay--isactive');
+			menuList.classList.toggle('menu__list--open');
+
+
+            menuItems.forEach(function (el, i) {
+                if (el.classList.contains("menu__item--animate-in")){
+                    el.classList.remove('menu__item--animate-in');
+                } else {
+                    setTimeout(function () {
+                        el.classList.add('menu__item--animate-in');
+                    }, 400 * i);
+                }
+            });
+		});
+
+		return this;
+	},
+
+
+	blogMenu: function() {
+		console.log("Hey");
+		return this;
 	}
-
-
-	function blogMenu() {
-		console.log(overlayRight);
-	}
-
-	 return {
-        mainMenu: mainMenu,
-        blogMenu: blogMenu
-    };
-})();
+}
 
 menuModule.mainMenu();
