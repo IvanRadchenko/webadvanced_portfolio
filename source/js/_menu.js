c39d2a0a946acd64a192be57ed90aa70319289f4
Ivan Radchenko <radchenk@gmail.com>
2016-10-27 10:29:03 +0300

    Change menu navigation animation

--- source/js/_menu.js
+++ source/js/_menu.js
@@ -1,6 +1,31 @@
-// var menuModule = (function () {
-//     //privates
-//     var menu = document.querySelector(".overlay");
-//
-//   }
-// )
+"use strict";
+var menuModule = {
+    mainMenu: function() {
+        var elem = document.querySelector(".overlay"),
+            hamburger = document.querySelector('.hamburger'),
+            menuList = document.querySelector('.menu__list'),
+            menuItems = document.querySelectorAll('.menu__item');
+
+        hamburger.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            hamburger.classList.toggle('hamburger--cross');
+            elem.classList.toggle('overlay--isactive');
+            menuList.classList.toggle('menu__list--open');
+
+            menuItems.forEach(function (el, i) {
+                el.classList.toggle('menu__item--animate-in');
+            });
+        });
+
+        return this;
+    },
+
+
+    blogMenu: function() {
+        console.log("Hey");
+        return this;
+    }
+};
+
+menuModule.mainMenu();
--- source/js/app.js
+++ source/js/app.js
@@ -1,43 +1,5 @@
 "use strict";
 
-// var menuModule = (function () {
-//   function mainMenu() {
-//     var elem = document.querySelector(".overlay"),
-//         hamburger = document.querySelector('.hamburger'),
-// 		menuList = document.querySelector('.menu__list'),
-// 		menuItems = document.querySelectorAll('.menu__item');
-//
-//
-//     hamburger.addEventListener('click', function(e) {
-// 			e.preventDefault();
-//
-//       hamburger.classList.toggle('hamburger--cross');
-// 		  elem.classList.toggle('overlay--isactive');
-// 		menuList.classList.toggle('menu__list--open');
-//
-// 		menuItems.forEach(function(i) {
-// 			var item = menuItems[i];
-// 			setTimeout(function() {
-// 				console.log(item);
-// 				item.classList.add('menu__item--animate-in');
-// 			}, 500 * i);
-// 		});
-// 	});
-// 	}
-//
-//
-// 	function blogMenu() {
-// 		console.log(overlayRight);
-// 	}
-//
-// 	 return {
-//         mainMenu: mainMenu,
-//         blogMenu: blogMenu
-//     };
-// })();
-//
-// menuModule.mainMenu();
-
 var menuModule = {
 	mainMenu: function() {
 		var elem = document.querySelector(".overlay"),
@@ -52,15 +14,8 @@ var menuModule = {
 			elem.classList.toggle('overlay--isactive');
 			menuList.classList.toggle('menu__list--open');
 
-
             menuItems.forEach(function (el, i) {
-                if (el.classList.contains("menu__item--animate-in")){
-                    el.classList.remove('menu__item--animate-in');
-                } else {
-                    setTimeout(function () {
-                        el.classList.add('menu__item--animate-in');
-                    }, 400 * i);
-                }
+                el.classList.toggle('menu__item--animate-in');
             });
 		});
 
@@ -72,6 +27,6 @@ var menuModule = {
 		console.log("Hey");
 		return this;
 	}
-}
+};
 
 menuModule.mainMenu();
